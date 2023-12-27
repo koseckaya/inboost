20df0e3e385271179ac3016b819e5480495882e3
koseckaya <kosetsaya@gmail.com>
2023-12-27 17:36:26 +0200

    init: initial packages

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Counter } from "./features/counter/Counter"
+import "./App.css"
+import Graph from "./components/Graph/Graph"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <Counter /> */}
+        <Graph />
+      </header>
+    </div>
+  )
+}
+
+export default App
